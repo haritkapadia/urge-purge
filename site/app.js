f48844e1d4ab28d766ecf1eac493412665931f68
starfy84 <dereck.tu@student.tdsb.on.ca>
2019-12-14 22:33:33 -0500

    POST

--- site/app.js
+++ site/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
 const bodyParser = require('body-parser')
 const app = express();
 const port = 3000;
+
+badSites=[]
 
 app.use(bodyParser());
 
@@ -11,6 +14,15 @@ app.use('/', express.static(__dirname));
 http.createServer(app).listen(port);
 
 
-app.post('/', function (req, resp) {
-  resp.end(JSON.stringify(req.body));
-})
+// app.get('/', function (req, res) {
+//   res.render('form-inline');
+//   res.sendFile('index.html');
+//   console.log(url.parse(req.body.link).host);
+// })
+
+app.post('/',function(req,res){
+  var link = url.parse(req.body.link).host;
+  badSites.push(link);
+  res.send('Test: ' +link);
+  console.log(badSites);
+});
