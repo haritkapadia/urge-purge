2c2205a2b12396d9b678cc1b23a3c48e5955eba4
starfy84 <dereck.tu@student.tdsb.on.ca>
2019-12-14 21:48:05 -0500

    Node server

---
+++ site/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const http = require('http');
+const fs = require('fs');
+const bodyParser = require('body-parser')
+const app = express();
+const port = 3000;
+
+app.use(bodyParser());
+
+app.use('/', express.static(__dirname));
+http.createServer(app).listen(port);
+
+
+app.post('/', function (req, resp) {
+  resp.end(JSON.stringify(req.body));
+})
+
+
